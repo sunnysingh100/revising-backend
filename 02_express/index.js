d231fa55dfe8d85eafc33c86d01bd0346e054a5c
Sunny Singh <sunnysingh706050@gmail.com>
2024-10-28 10:13:00 +0530

    add advance logging with morgan and winston

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,14 +1,36 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
+import logger from "./logger.js";
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
 
 // add a new tea
 app.post("/teas", (req, res) => {
+  logger.info("A post request was made to /teas");
+
   const {name, price} = req.body;
   const newTea = {
     id: nextId++,
@@ -57,6 +79,7 @@ app.delete("/teas/:id", (req, res) => {
   teaData.splice(index, 1);
   res.status(204).send();
 });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
